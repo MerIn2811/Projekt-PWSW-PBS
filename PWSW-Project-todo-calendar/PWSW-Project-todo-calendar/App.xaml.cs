fc7bd736efe0d8366092f344d4fc00839da58962
MerIn2811 <werlot000@pbs.edu.pl>
2025-12-14 22:43:22 +0100

    Created a Project
    Its an emty Project

---
+++ PWSW-Project-todo-calendar/PWSW-Project-todo-calendar/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PWSW_Project_todo_calendar;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
